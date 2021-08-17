9099d4b6eceea42bb9417215bdeac7185ab08041
AndreiR98 <89095452+AndreiR98@users.noreply.github.com>
2021-08-17 20:06:06 +0300

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,21 @@
+<?php
+date_default_timezone_get("Europe/Paris");
+
+$config['name'] = "Age of Vandals";
+$config['cdn'] = 'global_cdn';
+$config['Version'] = "0.0.1";
+$config['Server1'] = "BETA TEST";
+
+$config['host'] = "localhost";
+$config['user'] = "root";
+$config['password'] = "";
+$config['db'] = "aov";
+
+$config['density'] = 60;
+$config['radius'] = 3;
+
+$config['prefix'] = "s1_";
+$config['dimension'] = 80;
+
+$config['bonus'] = array('barracks', 'farm', 'garage', 'iron', 'stable', 'stone', 'storage', 'wood', 'all');
+?>
